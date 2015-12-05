96b330268b15043a403a324f9e0945fefbf5a954
Caleb Cauthon <calebcauthon@gmail.com>
2015-12-05 13:27:00 +1300

    Use sinatra

---
+++ public/app.js
@@ -0,0 +1,17 @@
+(function(){
+
+  function TestCtrl() {
+    this.user = {name: 'Blake'};
+  }
+
+  angular.module('app', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider.when('/', {
+      controller: 'TestCtrl as test',
+      templateUrl: 'test.html'
+    })
+    .otherwise('/');
+  })
+  .controller('TestCtrl', TestCtrl);
+
+})()
