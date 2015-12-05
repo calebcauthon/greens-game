f34371a5473b490a98a6f5681c424474c05c457a
Caleb Cauthon <calebcauthon@gmail.com>
2015-12-05 13:51:11 +1300

    Add basic structure for the quiz.

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/intro.html' do
+  send_file 'views/intro.html'
+end
+
+get '/question1.html' do
+  send_file 'views/question1.html'
+end
+
+get '/question2.html' do
+  send_file 'views/question2.html'
+end
+
+get '/score.html' do
+  send_file 'views/score.html'
+end
--- public/app.js
+++ public/app.js
@@ -1,17 +1,26 @@
 (function(){
 
-  function TestCtrl() {
+  function IntroCtrl($templateCache) {
     this.user = {name: 'Blake'};
   }
 
   angular.module('app', ['ngRoute'])
   .config(function($routeProvider){
     $routeProvider.when('/', {
-      controller: 'TestCtrl as test',
-      templateUrl: 'test.html'
+      controller: 'IntroCtrl as ctrl',
+      templateUrl: 'intro.html'
+    }).when('/question1', {
+      controller: function() {},
+      templateUrl: 'question1.html'
+    }).when('/question2', {
+      controller: function() {},
+      templateUrl: 'question2.html'
+    }).when('/score', {
+      controller: function() {},
+      templateUrl: 'score.html'
     })
     .otherwise('/');
   })
-  .controller('TestCtrl', TestCtrl);
+  .controller('IntroCtrl', IntroCtrl)
 
 })()
