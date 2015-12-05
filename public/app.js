23bf6d3b0e04ef512340b4d4cf087a2dd47c9525
Caleb Cauthon <calebcauthon@gmail.com>
2015-12-05 14:08:22 +1300

    Track answers using a service.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,33 @@
 (function(){
 
+  function AnswerSheetService() {
+    this.rightAnswersTally = 0;
+    this.wrongAnswersTally = 0;
+
+    function chooseRightAnswer() {
+      this.rightAnswersTally++;
+    }
+
+    function chooseWrongAnswer() {
+      this.wrongAnswersTally++;
+    }
+
+    this.chooseWrongAnswer = chooseWrongAnswer;
+    this.chooseRightAnswer = chooseRightAnswer;
+
+    return this;
+  }
+
   function IntroCtrl($templateCache) {
     this.user = {name: 'Blake'};
+  }
+
+  function AppCtrl($scope, AnswerSheet) {
+    $scope.chooseRightAnswer = function() { AnswerSheet.chooseRightAnswer(); };
+    $scope.chooseWrongAnswer = function() { AnswerSheet.chooseWrongAnswer(); };
+
+    $scope.rightAnswersTally = function() { return AnswerSheet.rightAnswersTally; };
+    $scope.wrongAnswersTally = function() { return AnswerSheet.wrongAnswersTally; };
   }
 
   angular.module('app', ['ngRoute'])
@@ -10,17 +36,20 @@
       controller: 'IntroCtrl as ctrl',
       templateUrl: 'intro.html'
     }).when('/question1', {
-      controller: function() {},
+      controller: 'AppCtrl as ctrl',
       templateUrl: 'question1.html'
     }).when('/question2', {
-      controller: function() {},
+      controller: 'AppCtrl as ctrl',
       templateUrl: 'question2.html'
     }).when('/score', {
-      controller: function() {},
+      controller: 'AppCtrl as ctrl',
       templateUrl: 'score.html'
     })
     .otherwise('/');
   })
   .controller('IntroCtrl', IntroCtrl)
+  .controller('AppCtrl', AppCtrl)
+
+  .service('AnswerSheet', AnswerSheetService)
 
 })()
